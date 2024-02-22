c2a9263a00bf5adcd7707a9b0decd469b1d21f40
rnoguer22 <rnogugon@myuax.com>
2024-02-22 22:01:20 +0100

    Hacemos el primer cluster de muchos xd

--- archivo.py
+++ archivo.py
@@ -6,26 +6,31 @@ class K_Means:
 
     def __init__(self, data_path):
         self.data_path = data_path
-    
-    def load_data(self):
-        df = pd.read_csv(self.data_path)
-        return df
 
-    def select_features(self, df, features):
-        X = df[features]
-        return X
     
-    def fit_model(self, X, n_clusters):
-        kmeans = KMeans(n_clusters=n_clusters)
-        kmeans.fit(X)
+    def fit_model(self, features, clusters:int):
+        df = pd.read_csv(self.data_path)
+        self.X = df[features]
+        kmeans = KMeans(n_clusters=clusters)
+        kmeans.fit(self.X)
         return kmeans
     
-    def visualize_results(self, X, labels, centroids):
+    
+    def plot_results(self, model):
+        centroids = model.cluster_centers_
         plt.figure(figsize=(8, 6))
-        plt.scatter(X['Feature1'], X['Feature2'], c=labels, cmap='viridis', s=50, alpha=0.5)
+        plt.scatter(self.X.iloc[:, 0], self.X.iloc[:, 1], c=model.labels_, cmap='viridis', s=50, alpha=0.5)
         plt.scatter(centroids[:, 0], centroids[:, 1], marker='*', s=300, c='red', label='Centroids')
-        plt.xlabel('Feature 1')
-        plt.ylabel('Feature 2')
+        plt.xlabel(self.X.columns[0])
+        plt.ylabel(self.X.columns[1])
         plt.title('K-Means Clustering')
         plt.legend()
         plt.show()
+
+
+kmean = K_Means('./UEFA_Analisis/UEFA_Final_Data.csv')
+model = kmean.fit_model(['W', 'Pts'], 3)
+kmean.plot_results(model)
+
+
+
