fa25057c73190b1a1636ef8b1aed3d6052f0cdcd
rnoguer22 <rnogugon@myuax.com>
2024-02-23 12:52:02 +0100

    Metodo fit_model

--- Clusters/DensityClustering/DensityCluster.py
+++ Clusters/DensityClustering/DensityCluster.py
@@ -1,7 +1,23 @@
 from Clusters.CentroidClustering.CentroidCluster import CentroidCluster
+from sklearn.cluster import DBSCAN, OPTICS, HDBSCAN
 
 
 class DensityCluster(CentroidCluster):
 
     def __init__(self, data_path, cluster_type:str='kmeans'):
         super().__init__(data_path, cluster_type)
+
+
+    #Metodo para encuadrar los datos en el modelo especificado
+    def fit_model(self, features, clusters:int=3):
+        self.X = self.df[features]
+        if self.cluster_type == 'dbscan':
+            model = DBSCAN(eps=0.3, min_samples=10)
+        elif self.cluster_type == 'optics':
+            model = OPTICS(min_samples=10, xi=0.05, min_cluster_size=0.05)
+        elif self.cluster_type == 'hdbscan':
+            model = HDBSCAN(min_cluster_size=15)
+        else:
+            raise ValueError('Invalid cluster type')
+        model.fit(self.X)
+        return model
