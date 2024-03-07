80ce141d440945c2080358beca825532714c5de5
rnoguer22 <rnogugon@myuax.com>
2024-03-07 14:21:24 +0100

    Separamos cada equipo con su propio csv

--- Spark/spark.py
+++ Spark/spark.py
@@ -1,5 +1,7 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, substring
+from pyspark.sql.functions import col
+import os
+import pandas as pd
 
 
 
@@ -9,6 +11,8 @@ class Spark:
         # Iniciamos una sesión de Spark, esto se hace siempre que se quiera trabajar con Spark
         self.spark = SparkSession.builder \
             .appName("Analisis_Champions_Spark") \
+            .config("spark.hadoop.fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem") \
+            .config("spark.hadoop.mapreduce.framework.name", "local") \
             .getOrCreate()
         
     def stop(self):
@@ -31,6 +35,18 @@ class Spark:
         teams = df.select(col_name).distinct().collect()
         lista_equipos = [row[col_name][:-10] for row in teams]
         return list(set(lista_equipos))
+    
+    def save_team(self, df, teams):
+        for team in teams:
+            filtered_df = df.filter(col('Squad').like('%'+team+'%'))
+            output_dir = './UEFA_Analisis_CSV/Equipos/'
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+                print("Carpeta creada exitosamente en: ", output_dir)
+            pandas_df = filtered_df.toPandas()
+            pandas_df.to_csv(output_dir + team + '.csv', index=False)
+
+
 
     #Metodo para hacer una regresion lineal de los datos utilizando machine learning de spark
     def mlregression(self):
@@ -43,7 +59,7 @@ df = spark.read_file('./UEFA_Analisis_CSV/UEFA_Final_Data.csv')
 print('\n\n\n')
 spark.df.printSchema()
 print('\n')
-print(spark.get_teams(df, 'Squad'))
+spark.save_team(df, spark.get_teams(df, 'Squad'))
 print('\n\n\n')
 
 spark.stop()
