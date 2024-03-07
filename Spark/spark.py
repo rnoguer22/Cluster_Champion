bff96625f9f1922607c381b15bcf7a0d9978dd6b
rnoguer22 <rnogugon@myuax.com>
2024-03-07 13:41:59 +0100

    Metodo para obtener una lista con los equipos de la champions

--- Spark/spark.py
+++ Spark/spark.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, substring
+
 
 
 class Spark:
@@ -15,34 +17,33 @@ class Spark:
     def read_file(self, path):
         if path[-3:] == 'csv':
             # Leemos el archivo csv
-            df = self.spark.read.csv(path, header=True, encoding='utf-8')
-            return df
-    
-    def schema(self, df):
-        df.printSchema()
-
-    def show(self, df):
-        df.show()
+            self.df = self.spark.read.csv(path, header=True, encoding='utf-8')
+            return self.df
     
     def create_temp_view(self, df, name):
         df.createOrReplaceTempView(name)
     
     def sql_query(self, query):
         return self.spark.sql(query).show()
+    
+    #Metodo para obtener una lista con los equipos de la champions
+    def get_teams(self, df, col_name):
+        teams = df.select(col_name).distinct().collect()
+        lista_equipos = [row[col_name][:-10] for row in teams]
+        return list(set(lista_equipos))
 
     #Metodo para hacer una regresion lineal de los datos utilizando machine learning de spark
     def mlregression(self):
         pass
     
 
-
+print('Ejecutando...')
 spark = Spark()
-df = spark.read_file('./UEFA_Predictions/csv/arima_Predictions.csv')
+df = spark.read_file('./UEFA_Analisis_CSV/UEFA_Final_Data.csv')
 print('\n\n\n')
-spark.schema(df)
-spark.show(df)
-spark.create_temp_view(df, 'arima')
-query = 'SELECT * FROM arima WHERE Prediction = "F"'
-result = spark.sql_query(query)
+spark.df.printSchema()
+print('\n')
+print(spark.get_teams(df, 'Squad'))
 print('\n\n\n')
+
 spark.stop()
