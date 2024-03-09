e9838387e53f7088e60e41c0ac81b605fe642c3a
rnoguer22 <rnogugon@myuax.com>
2024-03-10 00:27:06 +0100

    Guardamos prediccion en csv y creamos __init__.py

--- Spark/spark.py
+++ Spark/spark.py
@@ -16,28 +16,34 @@ class Spark:
             .config("spark.hadoop.fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem") \
             .config("spark.hadoop.mapreduce.framework.name", "local") \
             .getOrCreate()
+    
         
     def stop(self):
         self.spark.stop()
-        
+
+
     def read_file(self, path):
         if path[-3:] == 'csv':
             # Leemos el archivo csv
             self.df = self.spark.read.csv(path, header=True, encoding='utf-8')
             return self.df
     
+
     def create_temp_view(self, df, name):
         df.createOrReplaceTempView(name)
     
+
     def sql_query(self, query):
         return self.spark.sql(query).show()
     
+
     #Metodo para obtener una lista con los equipos de la champions
     def get_teams(self, df, col_name):
         teams = df.select(col_name).distinct().collect()
         lista_equipos = [row[col_name][:-10] for row in teams]
         return list(set(lista_equipos))
     
+
     def predict(self, df, teams):
         predictions = {}
         for team in teams:
@@ -52,6 +58,7 @@ class Spark:
         df_predictions.to_csv('./UEFA_Predictions/csv/LinearRegression_Predictions.csv', index=False)
         return df_predictions
 
+
     def linear_regression(self, df, target):
         rows = df.shape[0]
         #Variable dependiente
@@ -62,17 +69,16 @@ class Spark:
             prediction = 1
         #Variable objetivo
         y = list(map(int, df[target]))
-
         X_train = np.array(X).reshape(-1, 1)
         y_train = np.array(y)
         model = LinearRegression()
         model.fit(X_train, y_train)
-
-        # Predecir para nuevos valores de X
-        X_test = np.array([prediction]).reshape(-1, 1)  # Nuevos valores de X
+        #Predecimos para nuevos valores de X
+        X_test = np.array([prediction]).reshape(-1, 1)
         y_pred = model.predict(X_test)
         return self.convert(round(y_pred[0]))
     
+
     def convert(self, standing):
         standing = abs(round(standing))
         if standing == 0:
