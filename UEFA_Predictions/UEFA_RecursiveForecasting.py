d95358a6d0a0c451631c3ab99cba5bbc1186f0a6
rnoguer22 <rnogugon@myuax.com>
2024-03-28 01:29:21 +0100

    Obtenemos el jugador y su equipo, respectivamente

--- UEFA_Predictions/UEFA_RecursiveForecasting.py
+++ UEFA_Predictions/UEFA_RecursiveForecasting.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingRegressor
 
+from Spark.spark import Spark
 
 
-class RecursiveForecasting:
+
+class RecursiveForecasting(Spark):
 
     def __init__(self, data_path):
         self.data_path = data_path
@@ -18,6 +20,7 @@ class RecursiveForecasting:
 
         prediction_data = pd.read_csv(prediction_data_path, encoding='utf-8')
         teams = prediction_data['Squad'].values
+        copy_players = prediction_data[['id', 'Squad', 'Top Team Scorer', 'Goalkeeper']]
         prediction_data.drop(['Squad', 'id'], inplace=True, axis=1)
         prediction_data = prediction_data.iloc[:, :-2]
         X_pred = prediction_data.iloc[:, 1:]
@@ -29,11 +32,24 @@ class RecursiveForecasting:
             clf = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=1, random_state=0, loss='squared_error')
         clf.fit(X, y)
         y_pred = clf.predict(X_pred)
+        self.player_performance(copy_players, teams, y_pred)
         prediction_dict = dict(zip(teams, y_pred))
-        print(prediction_dict)
+                    
         sorted_prediction = self.convert(prediction_dict)
+        print(sorted_prediction)
         prediction_df = pd.DataFrame({'Squad':sorted_prediction.keys(), 'Prediction':sorted_prediction.values()})
         prediction_df.to_csv(f'./UEFA_Predictions/csv/{classifier}_Predictions.csv', index=False)
+
+
+    def player_performance(self, df_players, teams, predictions):
+        for team in teams:
+            players = df_players.loc[df_players['Squad'] == team]
+            scorer = players['Top Team Scorer'].iloc[-1][:-1]
+            for player in scorer.split(','):
+                player = player.replace('...', '')
+                print(player)
+            print(team[:-9])
+            print('\n')
 
 
     #Metodo para convertir el numero de standing a la ronda de la champions
