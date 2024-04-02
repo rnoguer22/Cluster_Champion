f2c4f980000e0660803deeda788eac060b495936
rnoguer22 <rnogugon@myuax.com>
2024-04-02 11:32:42 +0200

    Aplicamos zoom y rotaciones de los logos correctamente

--- Web_Scrapping/scrap_img.py
+++ Web_Scrapping/scrap_img.py
@@ -54,14 +54,28 @@ class Scrap_Img:
                     f.write(requests.get(img_url).content)
                     print(f'Imagen {img_name} del equipo {team_name} descargada exitosamente.')
                 
-                #Redimensionamos las imagenes tener mas datos para el modelo de prediccion de imagenes
+                #Redimensionamos las imagenes tener mas datos para el modelo de prediccion de imagenes    
                 for i in [0.75, 1, 1.25, 1.5]:
+                    img_original = Image.open(img_path)
+                    img_zoomed = img_original.copy()  
+                    # Calcula las nuevas dimensiones de la imagen después del zoom
+                    new_width = int(img_zoomed.width / i)
+                    new_height = int(img_zoomed.height / i)
+                    #Recortamos la imagen para hacer el zoom
+                    left = (img_zoomed.width - new_width) / 2
+                    top = (img_zoomed.height - new_height) / 2
+                    right = (img_zoomed.width + new_width) / 2
+                    bottom = (img_zoomed.height + new_height) / 2
+                    img_zoomed = img_zoomed.crop((left, top, right, bottom)) #Aplicamos el zoom
+                    #Redimensionamos la imagen y la guardamos
+                    img_zoomed = img_zoomed.resize((img_original.width, img_original.height))
+                    
+                    #Rotamos la imagen dimensionada
                     for r in [0, 90, 180, 270]:
-                        new_img = Image.open(img_path)
-                        new_img_name = team_name + '_' + str(i) + '_' + str(r) + '.png'
-                        new_img.thumbnail((int(new_img.size[0]*i), int(new_img.size[1]*i)))
-                        new_img.rotate(r)
-                        new_img.save(os.path.join(team_directory, new_img_name))
+                        img_rotated = img_zoomed.rotate(r, expand=True)
+                        new_img_name = f'{team_name}_{i}_{r}.png'
+                        img_rotated.save(os.path.join(team_directory, new_img_name))
+
 
 
     #Metodo para corregir los nombres de los equipos
