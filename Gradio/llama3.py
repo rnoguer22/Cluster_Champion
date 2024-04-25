784f177c1aaa23d1533c9f4997c574768b7c5fb8
rnoguer22 <rnogugon@myuax.com>
2024-04-26 00:07:59 +0200

    Primeras conversaciones con llama3 en python

--- Gradio/llama3.py
+++ Gradio/llama3.py
@@ -1,6 +1,34 @@
-from langchain_community.llms import Ollama
+'''from langchain_community.chat_models import ChatOllama
 
-llm = Ollama(model='llama3')
+llm = ChatOllama(model='llama3')
 
 result = llm.invoke('Hello World!')
-print(result)
+print(result)'''
+
+import requests
+
+url = "http://localhost:11434/api/chat"
+
+def llama3(prompt):
+    data = {
+        "model": "llama3",
+        "messages": [
+            {
+              "role": "user",
+              "content": prompt
+            }
+        ],
+        "stream": False
+    }
+    
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    
+    response = requests.post(url, headers=headers, json=data)
+    
+    return(response.json()['message']['content'])
+
+
+response = llama3("Tell me a joke!")
+print(response)
