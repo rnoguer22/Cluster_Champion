90a3a128e543c77c874d694ebed6f6b95c4d3314
rnoguer22 <rnogugon@myuax.com>
2024-04-26 11:18:41 +0200

    Usamos Blocks y Tabs de gradio

--- Gradio/llama3.py
+++ Gradio/llama3.py
@@ -21,15 +21,25 @@ def predict(prompt, history):
     response = requests.post(url, headers=headers, json=data)
     return(response.json()['message']['content'])
 
+def greet(name):
+    return "Hello " + name + "!"
+
 
 with gr.Blocks() as demo:
     gr.Markdown('''
     # Hola Mundo
+    
+    ## [Pincha aqui para acceder al link de este repositorio](https://github.com/rnoguer22/Cluster_Champion.git)
+                
     Estamos realizando pruebas con gradio
     ''')
-    input_text = gr.Textbox(lines=7, label="Input Text")
-    output_text = gr.Textbox(lines=7, label="Output Text")
-
-    gr.ChatInterface(predict)
+    with gr.Tabs():
+        with gr.TabItem('Prueba'):
+            input_text = gr.Textbox(lines=2, label="Input Text")
+            output_text = gr.Textbox(lines=2, label="Output Text")
+            text_button = gr.Button("Enviar")
+            text_button.click(greet, inputs=input_text, outputs=output_text)
+        with gr.TabItem('ChatBot'):
+            gr.ChatInterface(predict)
 
 demo.launch() 
